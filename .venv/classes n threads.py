f52cffe75d031029c8245545e0e3d67a33cfe7ea
Tamanda4 <t6433175l@gmail.com>
2025-04-28 23:54:12 +0300

    data setup and creating a table

---
+++ .venv/classes n threads.py
@@ -0,0 +1,15 @@
+class car :
+    def __init__(self,make,model,year,speed):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.speed = speed
+    def accelerate(self):
+        speed =0
+        if i += 1 :
+            i = speed
+         speed +=1
+
+
+
+
